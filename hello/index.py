7141d4767c58ca14f624f9d5d2d0a07cf1f208f4
Weten-Ogen <marcuoware@gmail.com>
2022-06-26 03:53:06 +0000

    this is pratice

--- hello/index.py
+++ hello/index.py
@@ -1,10 +1,38 @@
-def thing():
-    print('hello ')
-    print(' world')
+# largest_so_far = -1
 
-thing()
-print('zip')
-thing()
+# for i in [1,41,3,8,74,15]:
+#     if i > largest_so_far:
+#         largest_so_far = i
+#         print (largest_so_far, i)
+    
+# print("After", largest_so_far)
 
-two = max('two hundred')
-print(two)
+
+# Counting 
+# work = 0
+# print('Before', work)
+# for thing in  [9,42,12,3,74,15]:
+#     work = work + 1
+#     print(work , thing)
+# print('After' ,work)
+
+
+# count = 0
+# sum = 0 
+# print('Before', count , sum)
+# for value in [9, 41, 12, 3, 74, 15]:
+#     count = count + 1
+#     sum = sum + value
+#     print(count, sum, value)
+# print('After', count , sum, sum/count)
+
+
+
+
+
+
+
+
+
+
+
